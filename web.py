3551f781e435bb972d9dc99d8379bfa7201d67e7
bgsDev <bgs07dev@gmail.com>
2024-02-10 16:12:16 +0700

    add perhitungan partai

--- web.py
+++ web.py
@@ -58,7 +58,7 @@ def prosesData(pemilu,ddesa):
 
 
     dhasil = pd.Series(partai,index=nmPartai) 
-    for i,v in enumerate(dhasil): 
+    for i,v in enumerate(dhasil):
         for i1,v1 in enumerate(v):  # daftar Caleg
             totSuara = 0
             for i2,v2 in enumerate(ddesa):
@@ -78,7 +78,6 @@ def prosesData(pemilu,ddesa):
                 v1[ddesa.index[i2]+'Tot'] =totDesa
                 v1[ddesa.index[i2]+'TotTps'] =totalTps
             v1['totSuara'] = totSuara
-    
     return dhasil #pd.DataFrame(dhasil[0])[['no','nm','totSuara']]
 
 ddesa=getDesa(dpemilu)
@@ -87,19 +86,27 @@ dt = prosesData(dpemilu, ddesa)
 # print(dt[3][5])
 # print(pd.DataFrame(dt[0])[['totSuara','TaliwangTot','KertasariTot','DasanTot' ]])
 
+dpartai = []
 dtop1=[]
 for v in dt.index:
     ind = 0
+    totPartai = 0
     totSuara = 0
     for i1,v1 in enumerate(dt[v]):
         if(v1['totSuara']>totSuara):
             totSuara = v1['totSuara']
             ind = i1
+        totPartai+=v1['totSuara']
+
     dtop1.append({
         'Partai':v,
         'No Urut':dt[v][ind]['no'],
         'Nama Caleg':dt[v][ind]['nm'],
         'Total Suara':dt[v][ind]['totSuara']
+    })
+    dpartai.append({
+        'partai':v,
+        'total':totPartai
     })
 
 import streamlit as st
@@ -172,10 +179,26 @@ style ="""
     </style>
 """
 st.markdown(style, unsafe_allow_html=True)
+
+with st.container():
+    st.markdown("<div class='container1'></div>", unsafe_allow_html=True)
+    st.header(':sparkles:. Rangkuman Suara Partai')  
+
+    dgroup = pd.DataFrame(dpartai).groupby(by="partai").agg({
+        'total':'sum'
+    })['total'].sort_values(ascending=False)
+    col11, col22 = st.columns(2)
+    with col11:
+        tvCalegPartai =st.table(dgroup) 
+    with col22:
+        st.bar_chart(dgroup)
+
+
+
 container1 = st.container()
 container1.markdown("<div class='container1'>", unsafe_allow_html=True)
 with container1:
-    st.header('1. Daftar Suara Tertinggi Perwakilan Partai :sparkles:')
+    st.header(':sparkles:. Daftar Suara Tertinggi Perwakilan Partai ')
     st.dataframe(dtop1,use_container_width=True)
     container1.markdown("</div>", unsafe_allow_html=True)
 
@@ -183,7 +206,7 @@ with container1:
 
 with st.container():
     st.markdown("<div class='container1'></div>", unsafe_allow_html=True)
-    st.header('2. Daftar Caleg Berdasarkan Partai')   
+    st.header(':sparkles:. Daftar Caleg Berdasarkan Partai')   
     sdesa = st.selectbox("Pilih Opsi ", options=dt.index, key="partai",on_change=changePartai)
     col11, col22 = st.columns(2)
     with col11:
@@ -198,7 +221,7 @@ with st.container():
 # 3. menampilkan detail suara untuk setiap desanya 
 with st.container():
     st.markdown("<div class='container1'></div>", unsafe_allow_html=True)
-    st.header('3. Daftar Suara dari Desa / Kelurahan Berdasarkan Nama Caleg')   
+    st.header(':sparkles:. Daftar Suara dari Desa / Kelurahan Berdasarkan Nama Caleg')   
     sdesa = st.selectbox("Pilih Opsi Caleg", options=vcaleg, key="caleg",on_change=changeCaleg)
     col11, col22 = st.columns(2)
     with col11:
@@ -212,7 +235,7 @@ with st.container():
 # 4. menampilkan total setiap Tps dari setiap desa untuk tiap caleg
 with st.container():
     st.markdown("<div class='container1'></div>", unsafe_allow_html=True)
-    st.header('4. total suara tiap TPS dari setiap Desa / Kelurahan Berdasarkan Nama Caleg')   
+    st.header(':sparkles:. total suara tiap TPS dari setiap Desa / Kelurahan Berdasarkan Nama Caleg')   
     sdesa = st.selectbox("Pilih Opsi Desa", options=pd.DataFrame(vdesa)['desa'], key="desa",on_change=changeDesa)
     
     list =st.columns(4)
